0e93b247cac41d2a6013dc72619a55700e52ba25
RONNY <mboyaronny41@gmail.com>
2026-03-03 23:27:14 +0300

    backend polishing

--- app/extensions.py
+++ app/extensions.py
@@ -4,9 +4,13 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import os
 
 db      = SQLAlchemy()
 jwt     = JWTManager()
 bcrypt  = Bcrypt()
 migrate = Migrate()
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+    key_func=get_remote_address,
+    storage_uri=os.getenv("RATELIMIT_STORAGE_URL", "redis://localhost:6379/1")
+)
